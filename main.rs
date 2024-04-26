0e2b877e5f955eaed6d93e9035eefeb5b580d91e
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-04-26 09:48:08 -0300

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,9 @@
+use crate::{Multisig, Signer, SSI, BIP340, User, Proposal};
+
+use rgbstd::interface::{Rgb20, Fungible, Anchor, Balance, Amount};
+use ssdi::BIP340;
+use ssdi::Bitcoin;
+
+impl user for user = {
+    
+}
