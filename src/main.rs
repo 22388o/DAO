11f4cf1a7e0905580e2790cf6de3c2520fcb19aa
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-04-27 08:21:23 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,22 @@ use ssdi::BIP340;
 use ssdi::Bitcoin;
 
 impl user for user = {
+fn user = {
+let vote = let vote;
+let proposal = let proposal;
+let tx = let lext;
     
 }
-
+}
 impl Proposal for Proposal = {
-
+fn proposal = {
+let proposal = let proposal;
+let tx = let tx;
+}
+}
+impl RGB20 for RGB20 = {
+fn rgb20 = {
+let issuer = let issuer;
+let iface = let iface;
+let proposal = let proposal;
 }
